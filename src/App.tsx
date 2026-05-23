5eaddddaeddde9392f2fcfe85afe25b95a4d9220
uptokendev <uptokendev@gmail.com>
2026-05-23 16:39:41 +0200

    Replace recruiter redirects with in-app pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
-import ExternalRedirect from './components/ExternalRedirect'
 import SocialIdentityPanel from './components/SocialIdentityPanel'
+import RecruiterApplyPage from './pages/RecruiterApplyPage'
+import RecruiterPortalPage from './pages/RecruiterPortalPage'
 import WarAdminPage from './pages/WarAdminPage'
 import WarMissionsPage from './pages/WarMissionsPage'
 
@@ -13,8 +14,8 @@ export default function App() {
         <Route path="/missions/:section" element={<WarMissionsPage />} />
         <Route path="/profile/missions" element={<WarMissionsPage />} />
         <Route path="/profile/squad" element={<WarMissionsPage />} />
-        <Route path="/recruiter/apply" element={<ExternalRedirect label="Command Center recruiter signup" />} />
-        <Route path="/recruiter/portal" element={<ExternalRedirect label="Command Center recruiter tools" />} />
+        <Route path="/recruiter/apply" element={<RecruiterApplyPage />} />
+        <Route path="/recruiter/portal" element={<RecruiterPortalPage />} />
         <Route path="/admin/missions" element={<WarAdminPage />} />
         <Route path="/admin/missions/:section" element={<WarAdminPage />} />
         <Route path="/admin/notifications" element={<WarAdminPage />} />
